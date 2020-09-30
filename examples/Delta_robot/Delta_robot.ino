d40fe0a7c1f4fb11b07323ddb2d9dca0a5361e3d
Tinkers Projects <williambailes@gmail.com>
2020-09-30 22:13:44 +1000

    Add files via upload

---
+++ examples/Delta_robot/Delta_robot.ino
@@ -0,0 +1,45 @@
+#include <gcode.h>
+#include <DeltaKinematics.h> // Requires Delta Kinematics library from  https://github.com/tinkersprojects/Delta-Kinematics-Library
+
+void homing();
+gcode Commands();
+DeltaKinematics DK(70,300,139,112);
+
+void setup()
+{
+  Commands.begin("OK");  // ok, rs or !!
+}
+
+void loop() 
+{
+  if(Commands.available())
+  { 
+    if(Commands.availableValue('X'))
+    {
+      DK.x  = Commands.GetValue('X');
+    }
+
+    if(Commands.availableValue('Y'))
+    {
+      DK.y = Commands.GetValue('Y');
+    }
+
+    if(Commands.availableValue('Z'))
+    {
+      DK.z = Commands.GetValue('Z');
+    }
+      
+    DK.inverse();
+
+    //GOTO Position XYZ
+    // Move robot motors to DK.a, DK.b and DK.c CODE
+      
+    Commands.comment('X',DK.x);
+    Commands.comment('Y',DK.y);
+    Commands.comment('Z',DK.z);
+    Commands.comment('a',DK.a);
+    Commands.comment('b',DK.b);
+    Commands.comment('c',DK.c);
+
+  }
+}
